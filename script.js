2203c1a4400a389eab0da90e221dbb1489e263b2
HansChristian <hc.sev15@gmail.com>
2021-01-26 16:15:02 -0500

    Almost done.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const menuBars = document.querySelector(`.menuBars`);
-const headerUl = document.querySelector(`.headerList`);
 const headerFirstLi = document.querySelector(`.headerList li:first-child`);
+const headerUl = document.querySelector(`.headerList`);
 const logInButton = document.querySelector(`.headerDivs p`);
 const hiddenItems = document.querySelectorAll(`.hiddenItems`);
 
 // When the user clicks on the burger menu, the header list will slide open.
-function navShow(){
-    headerUl.classList.toggle(`navActive`);
-}
 menuBars.addEventListener(`click`, navShow);
 
 // When the user clicks on the "Close Menu" list item that's inside the header list, the menu will close.
 headerFirstLi.addEventListener(`click`, navShow);
 
+function navShow(){
+    headerUl.classList.toggle(`navActive`);
+}
+
+// Step 1: When the user clicks on "Login", the jumpDown function will fire. This function will not execute if the user clicks on the hamburger menu.
+logInButton.addEventListener(`click`, jumpDown);
 
 function jumpDown(){
     // Step 2: Jump to the bottom of the page.
@@ -27,13 +30,13 @@ function jumpDown(){
     })
 }
 
-// Step 1: When the user clicks on "Login", the jumpDown function will fire. This function will not execute if the user clicks on the hamburger menu.
-logInButton.addEventListener(`click`, jumpDown);
-
 // Step 5: When the user clicks on the "Close Menu" list item, the menu will reset to the original list and jump back to the top of the page.
+headerFirstLi.addEventListener(`click`, jumpUp);
+
 function jumpUp(){
     hiddenItems.forEach(function (hiddenItem) {
         hiddenItem.classList.remove(`displayItems`);
     })
 }
-headerFirstLi.addEventListener(`click`, jumpUp);
+
+// window.scrollTo(0, 0);
